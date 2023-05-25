af019850b83374db12717dfe5c81a20bd98c3370
Eduardo <eduardofreitasfr@gmail.com>
2023-05-25 01:37:03 -0300

    update parte responsivel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 
+
+
+// Selecione todos os links internos na página
+const links = document.querySelectorAll('scr');
+
+// Adicione um evento de clique suave a cada link
+links.forEach(link => {
+  link.addEventListener('click', smoothScroll);
+});
+
+// Função para rolagem suave
+function smoothScroll(event) {
+  event.preventDefault();
+
+  const targetId = this.getAttribute('href');
+  const targetElement = document.querySelector(targetId);
+
+  window.scrollTo({
+    top: targetElement.offsetTop,
+    behavior: 'smooth'
+  });
+}
+
+
+
+
+
 //Selecionar botao
 const btnservicos    = document.getElementById('btnservicos');
 
